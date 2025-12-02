f3ff2fdf30217980db66e91398b0d499bb6ad7a3
thiagopls1 <thiagopls1@hotmail.com>
2025-12-02 14:54:51 -0300

    fix player jumps

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,11 +6,14 @@ public class PlayerScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float speed = 5f;
     public float jmpForce = 1f;
+    public int maxJumps;
+    private int jumpsLeft;
     private Rigidbody2D rb;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpsLeft = maxJumps;
     }
 
     // Update is called once per frame
@@ -22,10 +25,19 @@ public class PlayerScript : MonoBehaviour
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput) * speed * Time.deltaTime;
         transform.Translate(movement);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0)
         {
             rb.linearVelocityY = 0;
             rb.AddForce(Vector2.up * jmpForce, ForceMode2D.Impulse);
+            jumpsLeft--;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            jumpsLeft = maxJumps; // Reseta os pulos quando toca no chão
         }
     }
 }
